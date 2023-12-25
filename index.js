0c457f110a591b424a90321e7afe3bc12ab1a2be
ido-lempert <il.mrbit@gmail.com>
2023-12-25 14:44:34 +0200

    c1

---
+++ index.js
@@ -0,0 +1,260 @@
+const express = require('express');
+const session = require('express-session');
+
+const crypto = require('crypto');
+
+const {
+    generateRegistrationOptions,
+    verifyRegistrationResponse,
+    generateAuthenticationOptions,
+    verifyAuthenticationResponse
+} = require('@simplewebauthn/server');
+
+const { isoBase64URL } = require('@simplewebauthn/server/helpers');
+
+const {json} = require("body-parser");
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const rpID = process.env.RENDER_EXTERNAL_HOSTNAME || 'localhost'
+const expectedOrigin = process.env.RENDER_EXTERNAL_URL || 'http://localhost:3000';
+const rpName = process.env.SITE_NAME || 'Passkey example';
+
+
+const users = [];
+const credentials = [];
+
+const auth = (req, res, next)=>{
+    if (req.session.username) {
+        next();
+    } else {
+        return res.sendStatus(403);
+    }
+
+}
+
+app.use(express.static('public'));
+
+app.use(session({
+    resave: true,
+    saveUninitialized: true,
+    secret: '1D0lEmP@r*',
+    cookie: { maxAge: 60000 }
+}));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/challenge', async (req, res) => {
+    try {
+        // Use SimpleWebAuthn's handy function to create a new authentication request.
+        const options = await generateAuthenticationOptions({
+            rpID: rpID,
+            allowCredentials: [],
+        });
+
+        // Keep the challenge value in a session.
+        req.session.challenge = options.challenge;
+
+        return res.json(options)
+    } catch (e) {
+        console.error(e);
+
+        return res.status(400).json({ error: e.message });
+    }
+});
+
+app.post('/register', json(), async (req, res)=>{
+    const user = {
+        id: isoBase64URL.fromBuffer(crypto.randomBytes(32)),
+        username: req.body.username,
+        password: req.body.password
+    };
+
+    users.push(user);
+    req.session.user = user;
+
+    try {
+        // Create `excludeCredentials` from a list of stored credentials.
+        const excludeCredentials = [];
+        const creds = credentials.filter(cred => cred.user_id === user.id);
+        for (const cred of creds) {
+            excludeCredentials.push({
+                id: isoBase64URL.toBuffer(cred.id),
+                type: 'public-key',
+                transports: cred.transports,
+            });
+        }
+        // Set `authenticatorSelection`.
+        const authenticatorSelection = {
+            authenticatorAttachment: 'platform',
+            requireResidentKey: true
+        }
+        const attestationType = 'none';
+
+        // Use SimpleWebAuthn's handy function to create registration options.
+        const options = await generateRegistrationOptions({
+            rpName: rpName,
+            rpID: rpID,
+            userID: user.id,
+            userName: user.username,
+            userDisplayName: user.displayName || user.username,
+            // Prompt users for additional information about the authenticator.
+            attestationType,
+            // Prevent users from re-registering existing authenticators
+            excludeCredentials,
+            authenticatorSelection,
+        });
+
+        // Keep the challenge value in a session.
+        req.session.challenge = options.challenge;
+
+        // Respond with the registration options.
+        return res.json(options);
+    } catch (e) {
+        return res.sendStatus(500);
+    }
+
+});
+
+/**
+ * Register a new passkey to the server.
+ */
+app.put('/register', json(), async (req, res) => {
+    // Set expected values.
+    const expectedChallenge = req.session.challenge;
+    // const expectedOrigin = getOrigin(req.get('User-Agent'));
+    // const expectedRPID = rpID;
+    const credential = req.body;
+
+    try {
+        // Use SimpleWebAuthn's handy function to verify the registration request.
+        const verification = await verifyRegistrationResponse({
+            response: credential,
+            expectedChallenge,
+            expectedOrigin,
+            expectedRPID : rpID,
+            requireUserVerification: false,
+        });
+
+        const { verified, registrationInfo } = verification;
+
+        // If the verification failed, throw.
+        if (!verified) {
+            throw new Error('User verification failed.');
+        }
+
+        const { credentialPublicKey, credentialID } = registrationInfo;
+
+        // Base64URL encode ArrayBuffers.
+        const base64PublicKey = isoBase64URL.fromBuffer(credentialPublicKey);
+        const base64CredentialID = isoBase64URL.fromBuffer(credentialID);
+
+        const user = req.session.user;
+
+        // Store the registration result.
+        credentials.push({
+            id: base64CredentialID,
+            publicKey: base64PublicKey,
+            // name: req.useragent.platform,
+            transports: credential.response.transports || [],
+            registered: (new Date()).getTime(),
+            last_used: null,
+            user_id: user.id,
+        });
+
+        // Delete the challenge from the session.
+        delete req.session.challenge;
+        delete req.session.user;
+
+        req.session.username = user.username;
+
+        // Respond with the user information.
+        return res.json(user);
+    } catch (e) {
+        delete req.session.challenge;
+        delete req.session.user;
+
+        console.error(e);
+        return res.status(400).send({ error: e.message });
+    }
+});
+
+app.put('/signin', json(), async (req, res) => {
+    // Set expected values.
+    const credential = req.body;
+    const expectedChallenge = req.session.challenge;
+    // const expectedOrigin = getOrigin(req.get('User-Agent'));
+    // const expectedRPID = process.env.HOSTNAME;
+
+    try {
+
+        // Find the matching credential from the credential ID
+        const cred = credentials.find(cred => cred.id === credential.id);
+        if (!cred) {
+            throw new Error('Matching credential not found on the server. Try signing in with a password.');
+        }
+
+        // Find the matching user from the user ID contained in the credential.
+        const user = users.find(user => user.id === cred.user_id);
+        if (!user) {
+            throw new Error('User not found.');
+        }
+
+        // Decode ArrayBuffers and construct an authenticator object.
+        const authenticator = {
+            credentialPublicKey: isoBase64URL.toBuffer(cred.publicKey),
+            credentialID: isoBase64URL.toBuffer(cred.id),
+            transports: cred.transports,
+        };
+
+        // Use SimpleWebAuthn's handy function to verify the authentication request.
+        const verification = await verifyAuthenticationResponse({
+            response: credential,
+            expectedChallenge,
+            expectedOrigin,
+            expectedRPID : rpID,
+            authenticator,
+            requireUserVerification: false,
+        });
+
+        const { verified, authenticationInfo } = verification;
+
+        // If the authentication failed, throw.
+        if (!verified) {
+            throw new Error('User verification failed.');
+        }
+
+        // Update the last used timestamp.
+        cred.last_used = (new Date()).getTime();
+
+        // Delete the challenge from the session.
+        delete req.session.challenge;
+
+        // Start a new session.
+        req.session.username = user.username;
+
+        return res.json(user);
+    } catch (e) {
+        delete req.session.challenge;
+
+        console.error(e);
+        return res.status(400).json({ error: e.message });
+    }
+});
+
+app.get('/transactions', auth, (req, res)=>{
+    res.json([
+        {
+            id: 777,
+            amount: 400
+        }
+    ]);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
---
+++ public/main.js
@@ -0,0 +1,177 @@
+const base64url = {
+    encode: function(buffer) {
+        const base64 = window.btoa(String.fromCharCode(...new Uint8Array(buffer)));
+        return base64.replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_');
+    },
+    decode: function(base64url) {
+        const base64 = base64url.replace(/-/g, '+').replace(/_/g, '/');
+        const binStr = window.atob(base64);
+        const bin = new Uint8Array(binStr.length);
+        for (let i = 0; i < binStr.length; i++) {
+            bin[i] = binStr.charCodeAt(i);
+        }
+        return bin.buffer;
+    }
+}
+
+function getChallenge(){
+    fetch('/challenge', {method: 'GET'}).then(res => {
+        console.log(res);
+    });
+}
+
+function submitForm(e){
+    this.event.preventDefault();
+    register(window.username.value, window.password.value).then(res => {
+        if (res.ok) {
+            window.location.href = '/transactions';
+        }
+    });
+}
+
+async function register(username, password){
+    if (!username || !password ) alert('ERROR');
+
+    const data = {
+        username: window.username.value,
+        password: window.password.value
+    };
+
+
+    const registerResponse = await fetch('/register', {method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify(data)});
+    const options = await registerResponse.json();
+
+    // Base64URL decode some values
+    options.user.id = base64url.decode(options.user.id);
+    options.challenge = base64url.decode(options.challenge);
+    if (options.excludeCredentials) {
+        for (let cred of options.excludeCredentials) {
+            cred.id = base64url.decode(cred.id);
+        }
+    }
+
+    // Use platform authenticator and discoverable credential
+    options.authenticatorSelection = {
+        authenticatorAttachment: 'platform',
+        requireResidentKey: true
+    }
+
+    if (abortController) {
+        abortController.abort({name: 'reAuthenticate'});
+        abortController = null;
+    }
+
+    // Invoke WebAuthn create
+    const cred = await navigator.credentials.create({
+        publicKey: options,
+    });
+
+    const credential = {};
+    credential.id = cred.id;
+    // Base64URL encode `rawId`
+    credential.rawId = base64url.encode(cred.rawId);
+    credential.type = cred.type;
+
+    // `authenticatorAttachment` in PublicKeyCredential is a new addition in WebAuthn L3
+    if (cred.authenticatorAttachment) {
+        credential.authenticatorAttachment = cred.authenticatorAttachment;
+    }
+
+    // Base64URL encode some values
+    const clientDataJSON = base64url.encode(cred.response.clientDataJSON);
+    const attestationObject = base64url.encode(cred.response.attestationObject);
+
+    // Obtain transports if they are available.
+    const transports = cred.response.getTransports ? cred.response.getTransports() : [];
+
+    credential.response = {
+        clientDataJSON,
+        attestationObject,
+        transports
+    };
+
+    // Send the result to the server and return the promise.
+    return await fetch('/register', {method: 'PUT', headers: {'Content-Type': 'application/json'}, body: JSON.stringify(credential)});
+}
+
+let abortController;
+async function authenticate(conditional = false){
+    // Fetch passkey request options from the server.
+    const challengeResponse = await fetch('/challenge');
+    const options = await challengeResponse.json();
+
+    // Base64URL decode the challenge
+    options.challenge = base64url.decode(options.challenge);
+
+    // `allowCredentials` empty array invokes an account selector by discoverable credentials.
+    options.allowCredentials = [];
+
+    // To abort a WebAuthn call, instantiate an `AbortController`.
+    abortController = new AbortController();
+
+    // Invoke WebAuthn get
+    const cred = await navigator.credentials.get({
+        publicKey: options,
+        signal: abortController.signal,
+        // Request a conditional UI
+        mediation: conditional ? 'conditional' : 'optional'
+    });
+
+    const credential = {};
+    credential.id = cred.id;
+    credential.type = cred.type;
+    // Base64URL encode `rawId`
+    credential.rawId = base64url.encode(cred.rawId);
+
+    // Base64URL encode some values
+    const clientDataJSON = base64url.encode(cred.response.clientDataJSON);
+    const authenticatorData = base64url.encode(cred.response.authenticatorData);
+    const signature = base64url.encode(cred.response.signature);
+    const userHandle = base64url.encode(cred.response.userHandle);
+
+    credential.response = {
+        clientDataJSON,
+        authenticatorData,
+        signature,
+        userHandle,
+    };
+
+    // Send the result to the server and return the promise.
+    return await fetch('/signin', {method: 'PUT', headers: {'Content-Type': 'application/json'}, body: JSON.stringify(credential)});
+}
+
+async function autoSignin(){
+    // Feature detection: check if WebAuthn and conditional UI are supported.
+    if (window.PublicKeyCredential &&
+        PublicKeyCredential.isConditionalMediationAvailable) {
+        try {
+            const cma= await PublicKeyCredential.isConditionalMediationAvailable();
+            if (cma) {
+                // If a conditional UI is supported, invoke the conditional `authenticate()` immediately.
+                const user = await authenticate(true);
+                if (user) {
+                    // When the user is signed in, redirect to the home page.
+                    // $('#username').value = user.username;
+                    // loading.start();
+                    // location.href = '/home';
+                } else {
+                    throw new Error('User not found.');
+                }
+            }
+        } catch (e) {
+            // loading.stop();
+            console.error(e);
+            // `NotAllowedError` indicates a user cancellation.
+            if (e.name !== 'NotAllowedError' && e.name !== 'reAuthenticate') {
+                alert(e.message);
+            }
+        }
+    }
+}
+
+function init(){
+    // getChallenge();
+    autoSignin();
+}
+
+init();
